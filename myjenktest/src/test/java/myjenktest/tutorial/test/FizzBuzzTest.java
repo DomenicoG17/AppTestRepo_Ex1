ea4c7f280a241c7cf5b2f4da3e36c2520075b2d7
DomenicoG17 <117814458+DomenicoG17@users.noreply.github.com>
2022-11-09 16:44:59 +0100

    initial

---
+++ myjenktest/src/test/java/myjenktest/tutorial/test/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package myjenktest.tutorial.test;
+
+import org.junit.jupiter.api.*; //con l'asterisco importiamo direttamente tutte le librerie necessarie
+
+import myjenktest.tutorial.FizzBuzz;
+
+public class FizzBuzzTest {
+	public FizzBuzz fb;
+	
+	@BeforeEach //fallo prima di ogni test
+	public void setup() {
+		fb=new FizzBuzz();
+	}
+	
+	@DisplayName("Play FizzBuzz with number=1")//ciò che comparirà nel report
+	@Test
+	public void testNumber() {
+		String risultato=fb.play(1);
+		Assertions.assertEquals(risultato,"1"); //compara il risultato ottenuto con quello pianificato
+												//se non coincidono, il test non va a buon fine
+	}
+
+}
